f1463bac7251e7df9ee1e482c8c0d3f62ac14b07
black <joseb.twelve@gmail.com>
2020-11-28 12:25:42 -0500

    fixed statement and exports of mongodb connection

--- model/db.js
+++ model/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(_ => console.log('DB is connected!'))
-    .catch(err => console.log("There're an error when connecting to DB", `ERROR: ${err}`));
-
-module.exports = mongoose;
+module.exports = () => {
+    mongoose.connect(
+        process.env.DATABASE_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then( _ => console.log('DB is connected!'))
+        .catch(err => console.log("There're an error when connecting to DB", `ERROR: ${err}`));
+}
