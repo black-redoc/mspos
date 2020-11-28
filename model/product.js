313e244c20979b4303b923e75cd73a0929c1f491
black <joseb.twelve@gmail.com>
2020-11-28 10:59:56 -0500

    fixed ProductSchema to a explicit declaration

--- model/product.js
+++ model/product.js
@@ -1,27 +1,27 @@
 const { model, Schema, SchemaTypes } = require('mongoose');
 
-const Product = model('Product', new Schema(
-    {
-        name: {
-            type: SchemaTypes.String,
-            required: true
-        },
-        price: {
-            type: SchemaTypes.Number,
-            required: true
-        },
-        stockApplies: {
-            type: SchemaTypes.Boolean,
-            required: true,
-            default: false
-        },
-        minimunStock: type: SchemaTypes.Number,
-        stock: SchemaTypes.Number,
-        code: {
-            type: SchemaTypes.Number,
-            required: true
-        }
+const ProductSchema = new Schema({
+    name: {
+        type: SchemaTypes.String,
+        required: true
+    },
+    price: {
+        type: SchemaTypes.Number,
+        required: true
+    },
+    stockApplies: {
+        type: SchemaTypes.Boolean,
+        required: true,
+        default: false
+    },
+    minimunStock: type: SchemaTypes.Number,
+    stock: SchemaTypes.Number,
+    code: {
+        type: SchemaTypes.Number,
+        required: true
     }
-));
+});
+
+const Product = model('Product', ProductSchema);
 
 module.exports = Product;
