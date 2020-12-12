20f8eb350093f9d12711e759957f41b6c7e5274d
black <joseb.twelve@gmail.com>
2020-12-12 17:16:18 -0500

    fixed bug on boolean isAuthenticated property

--- src/js/components/auth.js
+++ src/js/components/auth.js
@@ -2,16 +2,16 @@ export class Auth {
     constructor() {
         if (!localStorage.getItem('awe'))
             localStorage.setItem('awe', 0);
-        this.isAutheticated = new Boolean(localStorage.getItem('awe'));
+        this.isAutheticated = Boolean(Number(localStorage.getItem('awe')));
     }
 
     static login() {
         localStorage.setItem('awe', 1);
-        this.isAutheticated = new Boolean(localStorage.getItem('awe'));
+        this.isAutheticated = Boolean(Number(localStorage.getItem('awe')));
     }
 
     static logout() {
         localStorage.setItem('awe', 0);
-        this.isAutheticated = new Boolean(localStorage.getItem('awe'));
+        this.isAutheticated = Boolean(Number(localStorage.getItem('awe')));
     }
 }
