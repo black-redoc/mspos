5c3f71cdf4670ce87a140549ce4b6fef71d3c894
black <joseb.twelve@gmail.com>
2020-12-12 17:18:56 -0500

    added user icon menu on navbar

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { HashRouter as Router, Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import logo from '../../public/logo.png';
+import { Auth } from './auth';
+const { notificationApi } = electron;
 
 
 const NavBar = () => {
     const [burger_style, setBurgerStyle] = useState('navbar-menu')
     const [burger_active, setBurgerActive] = useState(false)
+    const history = useHistory();
 
     const toggleBurger = () => {
         const style = burger_active ? 'navbar-menu' : 'navbar-menu is-active';
@@ -14,63 +17,46 @@ const NavBar = () => {
     }
 
     const handleRedirect = () => toggleBurger();
+    const handleLogout = () => {
+        Auth.logout();
+        notificationApi.sendNotificacion({ title: 'Info', message: 'Se ha cerrado sesión' });
+        if (burger_active) toggleBurger();
+        history.push('/signin');
+    }
 
     return (
-        <nav className="navbar is-info" role="navigation" aria-label="main navigation">
+        <nav className="navbar is-fixed-top is-info" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
                 <Link className="navbar-item" to="/">
                     <img src={logo} className="logo" />
                 </Link>
 
-                <a role="button" onClick={toggleBurger} className="navbar-burger burger" aria-label="menu" aria-expanded="false">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
+                <a role="button" onClick={toggleBurger} className="navbar-burger burger mr-3" aria-label="menu" aria-expanded="false">
+                    <a role="button" href="#" className="button is-dark rounded mt-1">
+                        <i className="fas fa-user"></i>
+                    </a>
                 </a>
             </div>
 
             <div className={burger_style}>
-                <div className="navbar-start">
-                    <a className="navbar-item">
-                        Home
-                </a>
-
-                    <a className="navbar-item">
-                        Documentation
-                </a>
-
-                    <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link">
-                            More
-                    </a>
-
-                        <div className="navbar-dropdown">
-                            <a className="navbar-item">
-                                About
-                        </a>
-                            <a className="navbar-item">
-                                Jobs
-                        </a>
-                            <a className="navbar-item">
-                                Contact
-                        </a>
-                            <hr className="navbar-divider" />
-                            <a className="navbar-item">
-                                Report an issue
-                        </a>
-                        </div>
-                    </div>
-                </div>
-
                 <div className="navbar-end">
-                    <div className="navbar-item">
+                    <div className="navbar-item has-dropdown is-hoverable">
                         <div className="buttons">
-                            <Link to="/signup" className="button is-primary" onClick={handleRedirect}>
-                                <strong>Registrarse</strong>
-                            </Link>
-                            <Link to="/signin" className="button is-light" onClick={handleRedirect}>
-                                Iniciar Sesión
-                            </Link>
+                            <a role="button" onClick={toggleBurger} className="button is-dark rounded no-mobile" aria-label="menu" aria-expanded="true">
+                                <i className="fas fa-user"></i>
+                            </a>
+
+                            <div className="navbar-dropdown translatex-7">
+                                <div className="navbar-item">
+                                    <a href="#" onClick={()=>{}} role="button" className="button is-white">Ajustes</a>
+                                </div>
+                                <div className="navbar-item">
+                                    <a href="#" onClick={()=>{}} role="button" className="button is-white">Items</a>
+                                </div>
+                                <div className="navbar-item">
+                                    <a href="#" onClick={handleLogout} role="button" className="button is-white">Cerrar Sesión</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
