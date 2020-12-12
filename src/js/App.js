d7243af3d2851b9acc140e417bfa652cfbb24866
black <joseb.twelve@gmail.com>
2020-12-12 17:15:29 -0500

    deleted unnecesaty Redirect import

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import NavBar from './components/navbar';
 import SearchBar from './components/searchbar';
